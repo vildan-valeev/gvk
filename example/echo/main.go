0400069689bed953a6f148c54645a34d581c581b
Вильдан Валеев <vildan.maydanovich.valeev@gmail.com>
2024-01-21 20:24:18 +0500

    refactoring examples

--- example/echo/main.go
+++ example/echo/main.go
@@ -8,11 +8,12 @@ import (
 	"github.com/vildan-valeev/gvk"
 )
 
-const (
-	groupID      = 194299208
-	token        = "vk1.a.8-WDS15nXOxt3wU9pfCOHjAt0e7LiOZl8_u_su7PzFXVcgRgpJXZbDOB_cpBVKPOitdfTi9_Bp-oGQbrEeRr_ozjdHB3tbCtJOQFSe6VSEfX5C_IzvEUqZ3xnPlODyQTohFEh-EzhP9CcQEK5Ei6s0Xwy2L3JRSYIoup2wZFApIhpkGDTU5tdvQ09Skz7qMO3eg8fmqpM6jIzMfBIkZR6A"
-	tokenPosting = "vk1.a.F3PWupGyC4SKUFgQs51H1u7NOXcm8uWweVusWkmzWDwZha2uUzCeAItJGq4GBbwSQEnqIrHvVph6tt5xaHOc5w96q0GVKgJShvtUEIjtcEgj81CzXeaN8nJLEjrKN6ZVxpTU54Id45JOhY3sFBxg1giD45JxclmQXrk9FYxem6aRmXTpkSq-9hPnxuTyu5wb"
-)
+//
+//const (
+//	groupID      = 194299208
+//	token        = "vk1.a.8-WDS15nXOxt3wU9pfCOHjAt0e7LiOZl8_u_su7PzFXVcgRgpJXZbDOB_cpBVKPOitdfTi9_Bp-oGQbrEeRr_ozjdHB3tbCtJOQFSe6VSEfX5C_IzvEUqZ3xnPlODyQTohFEh-EzhP9CcQEK5Ei6s0Xwy2L3JRSYIoup2wZFApIhpkGDTU5tdvQ09Skz7qMO3eg8fmqpM6jIzMfBIkZR6A"
+//	tokenPosting = "vk1.a.F3PWupGyC4SKUFgQs51H1u7NOXcm8uWweVusWkmzWDwZha2uUzCeAItJGq4GBbwSQEnqIrHvVph6tt5xaHOc5w96q0GVKgJShvtUEIjtcEgj81CzXeaN8nJLEjrKN6ZVxpTU54Id45JOhY3sFBxg1giD45JxclmQXrk9FYxem6aRmXTpkSq-9hPnxuTyu5wb"
+//)
 
 type stateFn func(event *gvk.Update) stateFn
 
--- example/wall/main.go
+++ example/wall/main.go
@@ -7,18 +7,11 @@ import (
 	"strings"
 )
 
-const (
-	groupID      = 194299208
-	token        = "vk1.a.8-WDS15nXOxt3wU9pfCOHjAt0e7LiOZl8_u_su7PzFXVcgRgpJXZbDOB_cpBVKPOitdfTi9_Bp-oGQbrEeRr_ozjdHB3tbCtJOQFSe6VSEfX5C_IzvEUqZ3xnPlODyQTohFEh-EzhP9CcQEK5Ei6s0Xwy2L3JRSYIoup2wZFApIhpkGDTU5tdvQ09Skz7qMO3eg8fmqpM6jIzMfBIkZR6A"
-	tokenPosting = "vk1.a.aeQLjRCZN_9B7QfHcz7XJYeRuVr6ty6qgYP6QJ7k-qURVARGmeAFK-VEoIyoK1Qit6sRUxEcC71w7fxZJeUTKbyxY2MffdW8wSN4zU6zT1I31GeUM081O59pa8n_aVwx86QWXVYbP81TiEGd9GIoJ5V3mxyPgJDuGHT0FBlkhvjr1ZJjbGZ7EjJQPdZZ7BSQ"
-)
-
-/*
-https://api.vk.com/method/wall.post?access_token=vk1.a.F3PWupGyC4SKUFgQs51H1u7NOXcm8uWweVusWkmzWDwZha2uUzCeAItJGq4GBbwSQEnqIrHvVph6tt5xaHOc5w96q0GVKgJShvtUEIjtcEgj81CzXeaN8nJLEjrKN6ZVxpTU54Id45JOhY3sFBxg1giD45JxclmQXrk9FYxem6aRmXTpkSq-9hPnxuTyu5wb&v=5.131&message=pong&from_group=1&owner_id=-194299208
-
-https://oauth.vk.com/authorize?client_id=7306727&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.131
-
-*/
+//const (
+//	groupID      = 194299208
+//	token        = "vk1.a.8-WDS15nXOxt3wU9pfCOHjAt0e7LiOZl8_u_su7PzFXVcgRgpJXZbDOB_cpBVKPOitdfTi9_Bp-oGQbrEeRr_ozjdHB3tbCtJOQFSe6VSEfX5C_IzvEUqZ3xnPlODyQTohFEh-EzhP9CcQEK5Ei6s0Xwy2L3JRSYIoup2wZFApIhpkGDTU5tdvQ09Skz7qMO3eg8fmqpM6jIzMfBIkZR6A"
+//	tokenPosting = "vk1.a.aeQLjRCZN_9B7QfHcz7XJYeRuVr6ty6qgYP6QJ7k-qURVARGmeAFK-VEoIyoK1Qit6sRUxEcC71w7fxZJeUTKbyxY2MffdW8wSN4zU6zT1I31GeUM081O59pa8n_aVwx86QWXVYbP81TiEGd9GIoJ5V3mxyPgJDuGHT0FBlkhvjr1ZJjbGZ7EjJQPdZZ7BSQ"
+//)
 
 type stateFn func(event *gvk.Update) stateFn
 
@@ -68,13 +61,13 @@ func (b *Bot) EntryHandler(update *gvk.Update) stateFn {
 }
 
 func (b *Bot) handleNext(update *gvk.Update) stateFn {
-	poster := gvk.NewAPI(tokenPosting)
-	opt := gvk.WallEditOptions{
-		Message: "update pong",
-		OwnerID: -groupID,
-	}
-	_, err := poster.WallEdit(&opt)
-	log.Println(b.postID, err)
+	//poster := gvk.NewAPI(tokenPosting)
+	//opt := gvk.WallEditOptions{
+	//	Message: "update pong",
+	//	OwnerID: -groupID,
+	//}
+	//_, err := poster.WallEdit(&opt)
+	//log.Println(b.postID, err)
 	b.MessagesSend(&gvk.MessagesSendOptions{
 		Message: "pong again )))",
 		UserID:  b.chatID,
